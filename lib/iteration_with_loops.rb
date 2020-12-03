481c0337aa8c615f100a5d885db29ad49a042d7c
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-03 16:15:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,10 +3,10 @@ def find_even_values(src)
   #
   # Output all even values in each nested array
 array_arrays = [
-  [10, 11],
-  [99, 50, 3, 4],
-  [23, 41]
-  ]
+  #[10, 11],
+  #[99, 50, 3, 4],
+  #[23, 41]
+  #]
     row_index = 0
   while row_index < src.count do
     element_index = 0
